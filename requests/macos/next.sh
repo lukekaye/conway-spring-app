9587d6d7e2232d40e1db69a3686ab2adf4264fbd
lukekaye <12499643+lukekaye@users.noreply.github.com>
2026-08-16 20:35:53 +0100

    add taskfile for curl commands (cross platform)

--- requests/macos/next.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -s -X POST "http://localhost:8080/game/next" \
-  -H "Content-Type: application/json" \
-  -d '{"board":[[0,0],[0,0]],"boardName":"BLINKER","steps":-1}' \
-| jq "."
--- requests/windows/health.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-RestMethod `
-  -Uri "http://localhost:8080/actuator/health" `
-  -Method Get
